c6955f7031bb40b38cbb15520a67697afa7bc43b
DESKTOP-VRNOV73\camilo <andrescamiloaguirre568@gmail.com>
2018-02-25 20:06:12 -0500

    heroes and enemies

--- praxis/src/components/App/App.js
+++ praxis/src/components/App/App.js
@@ -3,12 +3,15 @@ import logo from './marvel-banner.png';
 import './App.css';
 import HeroeOverview from '../HeroeOverview/HeroeOverview';
 import heroesService from '../../services/heroesService';
+import enemiesService from "../../services/enemiesService";
+import EnemieOverview from "../EnemieOverview/EnemieOverview";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      heroes: []
+      heroes: [],
+      enemies: []
     };
   }
 
@@ -16,21 +19,33 @@ class App extends Component {
     heroesService.getHeroes().then((heroes) => {
       this.setState({ heroes: heroes.data });
     });
+    enemiesService.getHeroes().then((enemies) => {
+      this.setState({ enemies: enemies.data });
+    });
   }
 
   render() {
     let listHeroes = this.state.heroes.map((heroe) => <HeroeOverview key={heroe.name} name={heroe.name} image={heroe.image} overview={heroe.history} link={heroe.link} />);
+    let listEnemies = this.state.enemies.map((enemie) => <EnemieOverview key={enemie.name} name={enemie.name} image={enemie.image} overview={enemie.history} link={enemie.link} />);
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </header>
         <h2>Heroes</h2>
-        <div className="heroes-list">
+        <div className="character-list">
           {listHeroes}
         </div>
+         <br/><br/>
+        <h2>Enemies</h2>
+        <div className="character-list">
+           {listEnemies}
+         </div>
       </div>
+
     );
+
+
   }
 }
 
--- praxis/src/components/EnemieOverview/EnemieOverview.js
+++ praxis/src/components/EnemieOverview/EnemieOverview.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
-import './HeroeOverview.css';
+import './EnemieOverview.css';
 
-class HeroeOverview extends Component {
+class EnemieOverview extends Component {
     render() {
-        return (<div className='heroe-overview-container'>
+        return (<div className='enemie-overview-container'>
+
             <img src={this.props.image} alt={this.props.name} />
             <a href={this.props.link}><h1>{this.props.name}</h1></a>
-            <p className='heroe-overview-text'>{this.props.overview}</p>
+            <p className='enemie-overview-text'>{this.props.overview}</p>
             <a href={this.props.link}>More info..</a>
+
         </div>);
     }
 }
 
-export default HeroeOverview;
+export default EnemieOverview;
